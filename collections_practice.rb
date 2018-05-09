7ff0d2e2e05d8875b2e5ad90767d491a40992bc0
Anthony Chang <awchang12@gmail.com>
2018-05-09 23:00:14 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,49 @@
 # your code goes here
+def begins_with_r(arr)
+  boolean = true
+  arr.each do |x|
+    if x.start_with?("r")
+       boolean = true
+    else
+      boolean = false
+    end
+  end
+  return boolean
+end
+
+def contain_a(arr)
+  arr.select do |x|
+    x.include?("a")
+  end
+end
+
+def first_wa(arr)
+  arr.each do |str|
+    if str[0..1] == "wa"
+      return str
+    end
+  end
+end
+
+def remove_non_strings(arr)
+  arr.delete_if { |x|
+    x.class != String
+  }
+end
+
+def count_elements(arr)
+  temp = arr.uniq 
+  temp.each do |t|
+    count = 0
+    arr.each do |a|
+      if a == t 
+        count += 1
+      end 
+    end 
+    t[:count] = count
+  end
+end
+
+
+  
+  
