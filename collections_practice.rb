cf297d01472dc506f9a72aa9cc0cdaecf6c2ddf5
Anthony Chang <awchang12@gmail.com>
2018-05-09 23:16:07 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -79,5 +79,7 @@ def organize_schools(schools)
   schools.each do |school, location|
     location.each do |key, value|
       if organized_schools[value]
-        organized_schools[]
+        organized_schools[value] << school
+      else
+        organized_schools
 end
